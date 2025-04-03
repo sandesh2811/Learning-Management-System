73495840fdcae04d1a4134ac85aba32835c08be9
Sandesh <bhandarisandesh64@gmail.com>
2025-04-03 22:41:59 +0545

    Add redis connection , add initial session check middleware and add verify token

---
+++ src/middlewares/checkSession.ts
@@ -0,0 +1,69 @@
+import {
+    INTERNAL_SERVER_ERROR,
+    INTERNAL_SERVER_ERROR_MESSAGE,
+    UNAUTHORIZED,
+} from "@/constants/Constants";
+
+import { VerifySignedCookie } from "@/lib/cookie";
+import { VerifyJwtToken } from "@/lib/jwt";
+import { API_RESPONSE } from "@/utils/API_Response";
+
+import { JsonWebTokenError } from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+
+/*
+    Get the access token from the request/headers
+    If token not found then send respective response
+
+    If token is found (generally a signed token)
+    Verify the signature
+    If not verified send respective response
+
+    If token is verified (signature is verified) then verify the token (jwt token verification)
+    If token is invalid or expired send respective response
+
+    If token is valid then let the user continue to the required routes
+*/
+
+export const CheckSession = (req: NextRequest) => {
+    // Get access token
+    const access_token = req.headers.get("access_token");
+    console.log(access_token);
+
+    if (!access_token)
+        return API_RESPONSE(UNAUTHORIZED, {
+            success: false,
+            message: "Unauthorized!",
+        });
+
+    // Verify cookie signature
+    const token = VerifySignedCookie(access_token);
+
+    if (!token)
+        return API_RESPONSE(UNAUTHORIZED, {
+            success: false,
+            message: "Unauthorized!",
+        });
+
+    try {
+        // Verify jwt token
+        const user = VerifyJwtToken({
+            token: access_token,
+            tokenType: "access_token",
+        });
+
+        // Continue routing to specific api route
+        if (user) NextResponse.next();
+    } catch (error) {
+        if (error instanceof JsonWebTokenError) {
+            return API_RESPONSE(UNAUTHORIZED, {
+                success: false,
+                message: `Unauthorized! ${error.message}`,
+            });
+        }
+        return API_RESPONSE(INTERNAL_SERVER_ERROR, {
+            success: false,
+            message: INTERNAL_SERVER_ERROR_MESSAGE,
+        });
+    }
+};
