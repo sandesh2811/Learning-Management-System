024c60e3e47776530c7e58ca57e9d3532c08e5dc
Sandesh <bhandarisandesh64@gmail.com>
2025-06-03 09:41:23 +0545

    Add page level error and loading states and create page level error component

---
+++ src/app/courses/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import SingleCourseLoading from "@/features/singlecourse/components/SingleCourseLoading";
+
+const loading = () => {
+    return <SingleCourseLoading />;
+};
+
+export default loading;
